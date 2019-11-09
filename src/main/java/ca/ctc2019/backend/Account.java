74a76ff8dc949c121a6e3dcde97cf5e8dd4ab9be
TopazJ <shamez.meghji1@ucalgary.ca>
2019-11-09 13:05:57 -0700

    Added DatabaseController

--- src/main/java/ca/ctc2019/backend/Account.java
+++ src/main/java/ca/ctc2019/backend/Account.java
@@ -2,15 +2,33 @@ package ca.ctc2019.backend;
 
 public class Account {
 
+	private final int id;
 	private final String type;
 	private final String username;
 	private final String password;
 
 
-	public Account (String type, String username, String password) {
+	public Account (int id, String type, String username, String password) {
+		this.id = id;
 		this.type = type;
 		this.username = username;
 		this.password = password;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public String getUsername(){
+		return username;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public String getType(){
+		return type;
 	}
 
 }
