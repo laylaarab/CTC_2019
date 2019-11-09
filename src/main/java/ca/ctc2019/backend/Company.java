a27b63b3f2ca4d457e7f356e4689464e0f1fed33
TopazJ <shamez.meghji1@ucalgary.ca>
2019-11-09 13:33:59 -0700

    Added Industrial Item for when Companies Post Items

--- src/main/java/ca/ctc2019/backend/Company.java
+++ src/main/java/ca/ctc2019/backend/Company.java
@@ -7,11 +7,11 @@ public class Company {
 	private final String email;
 	private final Account account;
 
-	public Company (String name, String street, String city, String province, String postalCode, String email, String type, String username, String password){
+	public Company (String name, String street, String city, String province, String postalCode, String email, int id, String type, String username, String password){
 		this.name = name;
 		this.address = new Address(street, city, province, postalCode);
 		this.email = email;
-		this.account = new Account (type, username, password);
+		this.account = new Account (id, type, username, password);
 	}
 
 	public Company (String name, Address address, String email, Account account){
@@ -19,5 +19,21 @@ public class Company {
 		this.address = address;
 		this.email = email;
 		this.account = account;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public Address getAddress(){
+		return address;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public Account getAccount(){
+		return account;
 	}
 }
