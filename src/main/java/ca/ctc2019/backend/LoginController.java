92c7de9c682774b565ace89573d61e6108cb90c6
evan-krul <evan@krul.ca>
2019-11-09 13:34:07 -0700

    Added login option

---
+++ src/main/java/ca/ctc2019/backend/LoginController.java
@@ -0,0 +1,47 @@
+package ca.ctc2019.backend;
+
+import com.vaadin.flow.component.login.AbstractLogin;
+
+//Class to emulate login control
+public class LoginController {
+    enum AccountType {
+        COMPANY,
+        USER
+    }
+
+    private static LoginController instance;
+    private AccountType accountType;
+    private Boolean loggedIn;
+
+    private LoginController() {
+        accountType = null;
+        loggedIn = false;
+    }
+
+//    create login
+    public static LoginController getInstance()
+    {
+        if (instance == null)
+            instance = new LoginController();
+        return instance;
+    }
+
+    public Boolean loginUser(AbstractLogin.LoginEvent e) {
+//        TODO actually login
+        accountType = AccountType.COMPANY;
+        loggedIn = true;
+         return true;
+    }
+
+    public void logOut() {
+        accountType = null;
+        loggedIn = false;
+    }
+    public AccountType getAccountType() {
+        return accountType;
+    }
+    public Boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+}
